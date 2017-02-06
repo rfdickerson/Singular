91b1f80b86c7fd7399b7d0a0498feb23301c0e69
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-02-05 22:32:54 -0600

    More test cases

--- Tests/SingularTests/SingularTests.swift
+++ Tests/SingularTests/SingularTests.swift
@@ -51,12 +51,43 @@ class SingularTests: XCTestCase {
      
         let m = Matrix([[3, 0, 2], [2, 0, -2], [0, 1, 1]])
         
-        let a = Matrix([[0.2, 0.2, 0.0], [-0.2, 0.3, 1.0], [0.2, -0.3, 0.0]])
+        let a = Matrix([[0.2, 0.2, 0.0], [-0.2, 0.3, 1.0], [0.2, -0.3, -0.0]])
+        let minv = invert(m)
+        
+        XCTAssertEqual(minv!, a)
+        
+    }
+    
+    func testInvert2() {
+        
+        let m = Matrix([[1, 3, 3], [1, 4, 3], [1, 3, 4]])
+        
+        let a = Matrix([[7, -3, -3], [-1, 1, 0], [-1, 0, 1]])
         let minv = invert(m)
         
         XCTAssertEqual(minv, a)
         
     }
+    
+    func testInvert3() {
+        
+        let m = Matrix([[3, 0,  2, 1],
+                        [2, 0, -2, 3],
+                        [0, 1,  1, 2],
+                        [0, 0,  0, 1]])
+        
+        let a = Matrix([[ 0.2,  0.2,  0.0,  -0.8],
+                        [-0.2,  0.3,  1.0,  -2.7],
+                        [ 0.2, -0.3, -0.0,   0.7],
+                        [ 0.0,  0.0,  0.0,   1.0]])
+        
+        let minv = invert(m)
+        
+        XCTAssertEqual(minv!, a)
+        
+    }
+    
+    
 
 
     static var allTests : [(String, (SingularTests) -> () throws -> Void)] {
